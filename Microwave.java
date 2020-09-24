d03ba863049b2ce3baf333c5a101d96ed6036bde
bfalsetto <benjamin.falsetto@ocsbstudent.ca>
2020-09-24 18:06:28 +0000

    corrected formatting

--- Microwave.java
+++ Microwave.java
@@ -1,8 +1,17 @@
+
 /**
- * Class Untitled
- */
+* This application calculates the amount of energy that could be released.
+* from an object
+*
+* @author  Ben Falsetto
+* @version 1.0
+* @since   2020-09-22
+*/
 public class Microwave {
-	public static void main(String[] args) {
-    	System.out.println("Hello from AWS Cloud9!");
-	}
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Hello from AWS Cloud9!");
+    
+  }
 }
